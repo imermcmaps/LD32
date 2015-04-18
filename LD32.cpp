9226541dfe52cca9df2a6f381da85b15675bb9bc
Robin Rolf <imer@imer.cc>
2015-04-18 07:55:30 +0200

    Start button works

--- LD32.cpp
+++ LD32.cpp
@@ -4,13 +4,14 @@
 #include "TitleScreen.hpp"
 
 LD32::LD32() {
-	m_windowTitle ="LD32 - ???";
+	m_windowTitle ="LD32 - Vampire Slayer Deluxe (c) imer.cc 2015";
 	m_scene = engine::Factory::create<TitleScreen>("assets/scripts/title_menu.json", this);
+	engine::Factory::CreateChildFromFile("assets/scripts/loading_bg.json", &m_loadingScene);
 }
 
 LD32::~LD32() {
 }
 
 void LD32::StartGame() {
-	// TODO
+	SwitchScene(engine::Factory::create<engine::Scene>("assets/scripts/level_1.json", this));
 }
